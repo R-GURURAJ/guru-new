5ca33983b2127777aaf8c2979316fa3df460b5e1
R-GURURAJ <guru1guru0@gmail.com>
2022-11-10 18:29:09 +0530

    no

--- clas/author_class.java
+++ clas/author_class.java
@@ -1,7 +1,7 @@
 
-public class author_class {  
+public class author_class {
     public static void main(String[] args) {
-        Author a = new Author("guru","harish@gmail.com", 'm');   
+        Author a = new Author("guru", "harish@gmail.com", 'm');
         Book book = new Book("java full ref", a, 12.000, 21);
         book.getAuthor();
         book.getBookName();
@@ -9,62 +9,66 @@ public class author_class {
         book.setQtyInStock(21);
         book.getPrice();
         book.setPrice(23.900);
-        book.getPrice();        
+        book.getPrice();
     }
 }
+
 class Author {
     private String name;
     private String email;
     private char gender;
 
-    public Author(String name,String email,char gender) {
-        this.name=name;
-        this.email=email;
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
         this.gender = gender;
     }
-    public void hell(){
+
+    public void hell() {
         System.out.println(name);
         System.out.println(email);
         System.out.println(gender);
     }
 }
-class Book{
+
+class Book {
     private String bookname;
     private Author author;
     private double price;
     private int qtyInStock;
 
-    public Book(String bookname,Author author,double price,int qtyInStock) {
-        this.bookname=bookname;
-        this.price =price;
-        this.qtyInStock =qtyInStock;
-        this.author=author;
+    public Book(String bookname, Author author, double price, int qtyInStock) {
+        this.bookname = bookname;
+        this.price = price;
+        this.qtyInStock = qtyInStock;
+        this.author = author;
     }
 
-    void getBookName(){
-        String hi=this.bookname;
+    void getBookName() {
+        String hi = this.bookname;
         System.out.println(hi);
     }
-    
-    void getAuthor(){
-        Author hi=this.author;
+
+    void getAuthor() {
+        Author hi = this.author;
         hi.hell();
     }
-    
-    void getPrice(){
-        double hi=this.price;
+
+    void getPrice() {
+        double hi = this.price;
         System.out.println(hi);
     }
-    
-    void getQtyInStock(){
-        int hi=this.qtyInStock;
+
+    void getQtyInStock() {
+        int hi = this.qtyInStock;
         System.out.println(hi);
     }
-    
-    void setPrice(double price){
-        this.price=price;
+
+    void setPrice(double price) {
+        this.price = price;
     }
-    void setQtyInStock(int qtyInStock){
-        this.qtyInStock=qtyInStock;
+
+    void setQtyInStock(int qtyInStock) {
+        this.qtyInStock = qtyInStock;
     }
 }
